ad400ab43f734b1fae5e16d522b03ea4ffc2603d
Paul Armer <paul.armer@buildingconnected.com>
2020-08-31 16:59:51 -0700

    some create-react-app clean up

---
+++ src/shared-styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledBody = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap');
+  font-family: 'Fjalla One', sans-serif;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`
